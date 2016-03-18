598ca6f1a30a2dee523233b9ac4f3152ee5e231b
Tiberius <toddschmitt@gmail.com>
2016-03-18 08:00:33 -0500

    Deciding on final pics

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -7,56 +7,53 @@ var portfolio = {
 			cover : "img/stlukesmural.jpg",
 			pieces : [
 				{	
-					src : "img/stlukesmural.jpg",
-					title : "St. Luke's Mural", 
-					description : "some derelict appartments with grafitti",
-					long_description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-				}, 
-				{	
-					src : "img/stlukesmural_largewarm1.jpg",
-					title : "fountain", 
-					description : "a huge dragon fountain",
-					closeup_images : ["img/stlukesmural_largewarm1.jpg", "img/stlukemural_closeupcool1.jpg", "img/stlukemural_closeupwarm1.jpg", "img/stlukemural_closeupbright1.jpg"],
-					long_description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-				
-				}, 
-				{	
 					src : "img/stlukesmural_largecool1.jpg",
-					title : "tower", 
-					description : "a colourful tower block",
-					closeup_images : ["img/stlukesmural_largecool1.jpg", "img/stlukemural_closeupbright2.jpg","img/stlukemural_closeupbright3.jpg","img/stlukemural_closeupbright4.jpg","img/stlukemural_closeupbright5.jpg"],
-					long_description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-				
+					title : "Noah's Ark", 
+					description : "a huge dragon fountain",
+					closeup_images : ["img/stlukesmural_largecool1.jpg","img/stlukemural_closeupcool1.jpg","img/stlukemural_closeupbright2.jpg","img/stlukemural_closeupbright3.jpg","img/stlukemural_closeupbright4.jpg"]
+					
 				}, 
 				{	
-					src : "img/cake.png",
-					title : "walkways", 
-					description : "an interesting interior",
-					long_description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-				
+					src : "img/foo.jpg",
+					title : "School House", 
+					description : "a colourful tower block"
 				},
 				{	
-					src : "img/cabin.png",
-					title : "grafitti", 
-					description : "some derelict appartments with grafitti",
-					long_description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-				}, 
+					src : "img/foo.jpg",
+					title : "Construction Office", 
+					description : "a colourful tower block"
+				},
 				{	
-					src : "img/circus.png",
-					title : "fountain", 
-					description : "a huge dragon fountain",
-					long_description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-				
-				}, 
-				{	
-					src : "img/game.png",
-					title : "tower", 
-					description : "a colourful tower block",
-					long_description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-				
+					src : "img/foo.jpg",
+					title : "Nursery Rhyme Tea Party", 
+					description : "a colourful tower block"
 				}
 			]
 		},
+
+		{
+			name : "Old Master's Paintings", 
+			cover : "img/hoppercopy.jpg",
+			pieces : [
+				{	
+					src : "img/hoppercopy.jpg",
+					title : "dog in the snow", 
+					closeup_images : ["img/hoppercopy.jpg", "img/hoppercopy_Closeup1.jpg"],
+					description : "looks like he needs that jacket"
+				}, 
+				{	
+					src : "images/img_17.jpg",
+					title : "winter", 
+					description : "a snowy scene in a park"
+				}, 
+				{	
+					src : "images/img_18.jpg",
+					title : "frosty pond", 
+					description : "some ducks feeling cold"
+				} 
+			]
+		},
+
 		{
 			name : "Portraits", 
 			cover: "img/abbieportrait.jpg",
@@ -79,7 +76,7 @@ var portfolio = {
 			]
 		},
 		{
-			name : "Landscapes", 
+			name : "Chalk Art", 
 			cover : "img/hoppercopy.jpg",
 			pieces : [
 				{	
@@ -100,6 +97,7 @@ var portfolio = {
 				} 
 			]
 		}
+		
 
 	]
 };
